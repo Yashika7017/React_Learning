ff9d06cc21b565520ee08587ad45ee478d125598
Yashika Varshney <vyashika715@gmail.com>
2025-09-08 18:31:55 +0530

    learning react

---
+++ 03tailwindProps/src/App.jsx
@@ -0,0 +1,17 @@
+import Card from "./Components/card";
+
+function firstCss() {
+  
+  
+    return(
+        <>
+        <h1 className="bg-blue-400 text-black rounded-xl p-4 mb-4">Har Har Mahadev</h1>
+        <Card username="bored himanshu pic" />   
+         <Card />   
+        </>
+    )
+    
+}
+
+
+export default firstCss;
